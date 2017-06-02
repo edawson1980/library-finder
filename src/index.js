937a65deb787e774f28d56dbcbadf213aa2fa03c
Erin <erinedawson@gmail.com>
2017-06-02 11:40:59 -0400

    add component directories.  add library dummy data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App/App.js';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />,
+  document.getElementById('root'));
+
+
+const libraries = [
+  {name: "Mt. Pleasant" , address: "3160 16th St NW"},
+  {name: "MLK Memorial" , address: "901 G St NW"},
+  {name: "Petworth", address: "4200 Kansas Ave NW"},
+  {name: "Anacostia" , address: "1800 Good Hope Rd SE"},
+]
