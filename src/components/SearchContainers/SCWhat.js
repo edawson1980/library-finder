4d9c56b486c110e22fdad1d135e753afcd40a037
Erin <erinedawson@gmail.com>
2017-06-03 15:45:38 -0400

    create SCWhat js and css

---
+++ src/components/SearchContainers/SCWhat.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './SCWhat.css'
+
+class SCWhere extends Component {
+  render() {
+    let branches = this.props.branches.map((branch, index) => {
+      return(
+        <option key={index + 1} value={branch.name}>{branch.name}</option>
+      )
+    })
+    branches.unshift(
+      <option key="0">Pick your branch</option>
+    )
+        return(
+          <div className="menu">
+            <p>Please select a branch</p>
+            <select>
+              {branches}
+            </select>
+          </div>
+        )
+
+
+
+    }
+}
+
+export default SCWhere
