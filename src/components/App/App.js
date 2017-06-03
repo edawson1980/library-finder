862b96e88327822c499df720709778d6d2b412af
Erin <erinedawson@gmail.com>
2017-06-03 16:25:47 -0400

    add axios import to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 import SCWhere from '../SearchContainers/SCWhere.js'
 import SCWhat from '../SearchContainers/SCWhat.js'
 import {
