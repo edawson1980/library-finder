7e721e5e5f3756571c7f6e62dcc5458d8d4c955b
Erin <erinedawson@gmail.com>
2017-06-07 18:01:25 -0400

    create index.js, with App.js imported

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Post from './components/App/App.js';
-
-var post = {
-  title: "Hungry Mouse",
-  author: "Erin",
-  body: "I'm hungry.  But also lazy.  And broke.",
-  comments: [
-    "get a job",
-    "quit whining",
-    "drink some water",
-    "probably make some pasta"
-  ]
-}
+import App from './components/App/App';
+import './index.css'
 
 
-ReactDOM.render(
-  <Post
-    title={post.title}
-    author={post.author}
-    body={post.body}
-    comments={post.comments}
-  />,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
