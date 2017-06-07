5d2bd4d872faec0a2d467e855c6a0835d53e56b1
Erin <erinedawson@gmail.com>
2017-06-07 19:38:42 -0400

    create search container for locations (SCWhere)

---
+++ src/components/SCWhere/SCWhere.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import SearchWhere from './SearchWhere'
+import RWhere from './RWhere'
+import {queryLocations} from '../Utils'
+
+
+
+class SCWhere extends Component {
+  constructor(props){
+    super(props)
+    this.onChange = this.onChange.bind(this)
+    this.state = {
+      query: '',
+      hasSearched: false,
+      locations: []
+    }
+  }
+
+  onChange(menuLocation){
+    queryLocations(menuLocation).then((locations) => {
+      this.setState({
+        locations: locations,
+        hasSearched: true
+      })
+    })
+  }
+
+  render (){
+    return (
+      <div className="SCWhere">
+        {
+          this.state.hasSearched ?
+
+          <RWhere
+            locations = {this.state.locations}
+          />:
+
+          <SearchWhere
+            onChange = {this.onChange}
+            query = {this.state.query}
+          />
+        }
+
+      </div>
+    )
+  }
+}
+
+export default SCWhere
