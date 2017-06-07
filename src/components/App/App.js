e7d085d716033e00562fdb3174784914d603b8f4
Erin <erinedawson@gmail.com>
2017-06-07 18:06:01 -0400

    create App.js, with axios and router imported

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,49 +6,18 @@ import {
   Link,
 } from 'react-router-dom'
 
-import SCWhere from '../SearchContainers/SCWhere.js'
-import SCWhat from '../SearchContainers/SCWhat.js'
-import Comment from './Comment.js'
-
 import './App.css';
 
-class Post extends Component {
-  constructor (props) {
-    super ()
-    this.state = {
-      body: props.body
-    }
-  }
-  handleClick (e) {
-    let newBody = prompt("What would you like to see here?")
-    this.setState ({
-      body: newBody
-      })
-    }
-  
+class App extends Component {
   render (){
-    let comments = this.props.comments.map((comment, index) => (
-      <Comment body={comment} key={index} />
-    ))
-    // this says go through each comment in the array comments,
-    // and then run the map method over it, using the two arguments comment and index.
-    // then, plug the Comment component in (but assign it to the variable comments).
-    // and the reason you have props.body in Comment.js is because it is referencing the body value here
     return (
       <div>
-        <h1>{this.props.title}!</h1>
-        <h3>by: {this.props.author}</h3><br/>
-        <p></p>
-        <p>{this.state.body}</p>
-        <button onClick ={(e) => this.handleClick(e)}>Edit body</button>
-        <br/>
-        <h3>Comments:</h3>
-        {comments}
+        <h1>Welcome to BookWorm!</h1>
+        <h3>Your new fave app for reading resources in the District and beyond.</h3>
       </div>
     )
   }
 }
 
 
-
-export default Post
+export default App
